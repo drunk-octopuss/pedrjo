096d47525eb552db8d7c63b706a792aaa62380fe
Elliot Cuhrt <ellcuh@gmail.com>
2022-12-11 14:58:32 +0200

    get+push push error "text.push is not a function"

--- Elliot/routes/userRoutes.js
+++ Elliot/routes/userRoutes.js
@@ -2,9 +2,37 @@ const express = require("express");
 const router = express.Router();
 
 //const controller = require("../controller/userController");
-
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "hello elliot" });
+var text = [
+  {
+    id: "1",
+    text: "hello!",
+    font: "Futura",
+  },
+];
+router.get("/contact", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "data fatched successfully",
+    data: text,
+  });
+});
+router.post("/contact", (req, res) => {
+  var text = req.body.text;
+  if (text) {
+    text.push({
+      id: (text.length + 1).toString(),
+      text: text,
+    });
+    res.send({
+      success: true,
+      message: "data added successfully",
+    });
+  } else {
+    res.send({
+      success: false,
+      message: "validation error",
+    });
+  }
 });
 
 module.exports = router;
